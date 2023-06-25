14865ca533c5ed69ce21068907248e37a49737ac
Saani765 <saani765@gmail.com>
2023-06-25 22:21:35 +0530

    added a new solved problem

---
+++ maximum-in-subarray.java
@@ -0,0 +1,29 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int[] ans=new int[nums.length-k+1];
+        Deque<Integer> dq=new ArrayDeque<>();
+        int i=0;
+        int j=0;
+        while(j<nums.length)
+        {
+            if(dq.size()==0) dq.add(nums[j]);
+            else
+            {
+                while(dq.size()!=0 && nums[j]>dq.peekLast()) dq.pollLast();
+                dq.add(nums[j]);
+            }
+            if(j-i+1==k)
+            {
+                ans[i]=dq.peek();
+                if(nums[i]==dq.peek()) dq.pollFirst();
+                i++;
+
+            }
+            j++;
+        }
+        return ans;
+    }
+}
