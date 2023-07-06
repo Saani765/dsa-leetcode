dabaac2497c33bdf32fb15866e715ba7005e8ec2
Saani765 <saani765@gmail.com>
2023-07-06 16:06:19 +0530

    added new solved problem for dp

---
+++ Dynamic Programming/Partitions with Given Difference.java
@@ -0,0 +1,42 @@
+class Solution{
+    public int perfectSum(int arr[],int n, int sum) 
+	{ 
+	    // Your code goes here
+	    int mod = (int)1e9+7;
+	    int[][] t=new int [n+1][sum+1];
+	    for(int i=0;i<n+1;i++)
+	    {
+	        for(int j=0;j<sum+1;j++)
+	        {
+	            if(i==0) t[i][j]=0;
+	            if(j==0) t[i][j]=1;
+	        }
+	    }
+	     for(int i=1;i<n+1;i++)
+	    {
+	        for(int j=0;j<sum+1;j++)
+	        {
+	           if(arr[i-1]<=j)
+	           {
+	               t[i][j]= (t[i-1][j]%mod) + (t[i-1][j-arr[i-1]]%mod);
+	           }
+	           else t[i][j]=t[i-1][j]%mod;
+	            
+	            
+	        }
+	    }
+	    return t[n][sum]%mod;
+	} 
+
+    public int countPartitions(int n, int d, int arr[]){
+        // Code here
+        int s=0;
+        for(int i=0;i<n;i++)
+        {
+            s+=arr[i];
+        }
+        if((s+d)%2!=0 || s<0) return 0;
+        return perfectSum(arr,n,(s+d)/2);
+        
+    }
+}
