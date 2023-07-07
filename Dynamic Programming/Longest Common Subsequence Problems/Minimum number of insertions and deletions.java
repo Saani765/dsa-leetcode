075beea7e1dff0183153a5e695e26815a7a3b58f
Saani765 <saani765@gmail.com>
2023-07-07 23:04:30 +0530

    new solved problem related to lcs

---
+++ Dynamic Programming/Longest Common Subsequence Problems/Minimum number of insertions and deletions.java
@@ -0,0 +1,29 @@
+class Solution
+{
+	public int minOperations(String text1, String text2) 
+	{ 
+	    // Your code goes here
+	    int m=text1.length();
+        int n=text2.length();
+        int [][] t=new int[m+1][n+1]; 
+        for(int i=0;i<m+1;i++)
+        {
+            for(int j=0;j<n+1;j++)
+            {
+                if(i==0 || j==0) t[i][j]=0;
+            }
+        }
+        for(int i=1;i<m+1;i++)
+        {
+            for(int j=1;j<n+1;j++)
+            {
+                if(text1.charAt(i-1)==text2.charAt(j-1))
+                t[i][j]=1+t[i-1][j-1];
+                else
+                t[i][j]=Math.max(t[i-1][j],t[i][j-1]);
+            }
+        }
+        int len=t[m][n];
+        return (m+n)-(2*len);
+    }
+	} 
