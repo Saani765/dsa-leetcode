251100c1bafd65266ee8d4091842277107567f43
Saanidhya Singh <98387792+Saani765@users.noreply.github.com>
2023-07-29 14:40:28 +0530

    Time: 0 ms (100%), Space: 39.2 MB (100%) - LeetHub

---
+++ 0826-soup-servings/0826-soup-servings.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double soupServings(int n) {
+        if(n>4800) return 1;
+         n=(int)Math.ceil(n*1.0/25);
+        double dp[][]= new double[n+1][n+1];
+        return helper(n,n,dp);
+    }
+    double helper(int a,int b,double dp[][]){
+        if(a<=0 && b<=0) return 0.5;
+        if(b<=0) return 0;
+        if(a<=0) return 1;
+        if(dp[a][b]>0) return dp[a][b];
+        return dp[a][b]=0.25*(helper(a-4,b,dp)+helper(a-3,b-1,dp)+helper(a-2,b-2,dp)+helper(a-1,b-3,dp));
+    }
+}
